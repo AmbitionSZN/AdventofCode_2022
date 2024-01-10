5c5ae1990911c10c92ba4dbc28d1f678dda8887c
AmbitionSZN <jahlwalk22635@gmail.com>
2024-01-09 20:23:18 -0500

    finished day2

--- Day2/main.go
+++ Day2/main.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-func getTotalScore(rounds [][]rune) int {
+func partOne(rounds [][]rune) int {
 	totalScore := 0
 	for _, v := range rounds {
 		fmt.Println(v)
@@ -45,6 +45,42 @@ func getTotalScore(rounds [][]rune) int {
 	return totalScore
 }
 
+func partTwo(rounds [][]rune) int {
+	totalScore := 0
+	for _, v := range rounds {
+		switch v[0] {
+		case 65:
+			switch v[2] {
+			case 88:
+				totalScore += 3
+			case 89:
+				totalScore += 1 + 3
+			case 90:
+				totalScore += 2 + 6
+			}
+		case 66:
+			switch v[2] {
+			case 88:
+				totalScore += 1
+			case 89:
+				totalScore += 2 + 3
+			case 90:
+				totalScore += 3 + 6
+			}
+		case 67:
+			switch v[2] {
+			case 88:
+				totalScore += 2
+			case 89:
+				totalScore += 3 + 3
+			case 90:
+				totalScore += 1 + 6
+			}
+		}
+	}
+	return totalScore
+}
+
 func parseInput() [][]rune {
 	content, err := os.ReadFile("./puzzleinput.txt")
 	if err != nil {
@@ -63,6 +99,6 @@ func parseInput() [][]rune {
 }
 
 func main() {
-	output := getTotalScore(parseInput())
+	output := partTwo(parseInput())
 	fmt.Println(output)
 }
