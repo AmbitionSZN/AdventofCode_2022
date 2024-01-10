faec83a18e3557b67b585d6fc042ac8fe9ab960d
AmbitionSZN <jahlwalk22635@gmail.com>
2024-01-09 23:55:14 -0500

    finished day 3 part 1

---
+++ Day3/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func parseInput() [][]rune {
+	content, err := os.ReadFile("./puzzleinput.txt")
+
+	if err != nil {
+		log.Fatalln("error reading file")
+	}
+
+	data := strings.Split(string(content), "\r\n")
+	runeData := make([][]rune, len(data))
+
+	for i, v := range data {
+		runeData[i] = []rune(v)
+	}
+	return runeData
+}
+
+func sumItemTypes(rucksacks [][]rune) int {
+	matches := []rune{}
+	for _, v := range rucksacks {
+		compartmentOneLen := len(v) / 2
+		compartmentOne := v[:compartmentOneLen]
+		compartmentTwo := v[compartmentOneLen:]
+		//fmt.Println(compartmentOne)
+		//fmt.Println(compartmentTwo)
+		//fmt.Printf("%v\naaaaaa", v)
+		for _, valueOne := range compartmentOne {
+			bool := false
+			for _, valueTwo := range compartmentTwo {
+				if valueOne == valueTwo {
+					//fmt.Println(valueOne)
+					matches = append(matches, valueOne)
+					bool = true
+					break
+				}
+			}
+			if bool {
+				break
+			}
+		}
+	}
+
+	fmt.Println(matches)
+	total := 0
+
+	for _, v := range matches {
+		priority := int(v) - 96
+		if priority < 1 {
+			total += priority + 58
+		} else {
+			total += priority
+		}
+	}
+	return total
+}
+
+func main() {
+	fmt.Println(sumItemTypes(parseInput()))
+}
