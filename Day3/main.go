a3f41aa1178c3bead11f313be3b354d2079063a6
AmbitionSZN <jahlwalk22635@gmail.com>
2024-01-11 21:16:10 -0500

    finished day3

--- Day3/main.go
+++ Day3/main.go
@@ -58,6 +58,50 @@ func sumItemTypes(rucksacks [][]rune) int {
 	return total
 }
 
+func partTwo(rucksacks [][]rune) int {
+	matches := []rune{}
+
+	for i := 0; i < len(rucksacks); {
+		for _, itemTypeOne := range rucksacks[i] {
+			count := 0
+			for _, itemTypeTwo := range rucksacks[i+1] {
+				if itemTypeOne == itemTypeTwo {
+					count++
+					break
+				}
+			}
+			if count == 1 {
+				for _, itemTypeThree := range rucksacks[i+2] {
+					if itemTypeOne == itemTypeThree {
+						matches = append(matches, itemTypeOne)
+						count++
+						break
+					}
+				}
+			}
+			if count == 2 {
+				break
+			}
+		}
+		i += 3
+	}
+	fmt.Println(matches)
+	return getSum(matches)
+}
+
+func getSum(arr []rune) int {
+	total := 0
+	for _, v := range arr {
+		priority := int(v) - 96
+		if priority < 1 {
+			total += priority + 58
+		} else {
+			total += priority
+		}
+	}
+	return total
+}
+
 func main() {
-	fmt.Println(sumItemTypes(parseInput()))
+	fmt.Println(partTwo(parseInput()))
 }
